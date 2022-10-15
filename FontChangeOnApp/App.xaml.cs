a1d23630826b3fa65b4a1a91dccdab92fda758d1
rtazaki <rtazaki@gmail.com>
2022-10-16 00:08:47 +0900

    フォント設定をxamlに頼らずにアプリ起動時に一括設定する。

--- FontChangeOnApp/App.xaml.cs
+++ FontChangeOnApp/App.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace FontChangeOnApp
 {
@@ -13,5 +10,19 @@ namespace FontChangeOnApp
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            // WindowやPage毎にスタイルを追加する必要がある。
+            Action<FontFamily, double, Type> style = (font, size, type) =>
+            {
+                var style = new Style(type);
+                style.Setters.Add(new Setter(Control.FontFamilyProperty, font));
+                style.Setters.Add(new Setter(Control.FontSizeProperty, size));
+                FrameworkElement.StyleProperty.OverrideMetadata(
+                    type, new FrameworkPropertyMetadata(style));
+            };
+            style(new FontFamily("MS Gothic"), 42.0, typeof(Window));
+            style(new FontFamily("HGP創英角ﾎﾟｯﾌﾟ体"), 24.0, typeof(Page));
+        }
     }
 }
---
+++ FontChangeOnApp/View/Page1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FontChangeOnApp.View
+{
+    /// <summary>
+    /// Page1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
